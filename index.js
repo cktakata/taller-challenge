64205ad418b8a96e2929a6eaaf6fcc3fd888007d
Carlos Takata <carlos.takata@gmail.com>
2025-03-04 11:47:34 -0300

    second release

--- index.js
+++ index.js
@@ -16,3 +16,50 @@
 // Output: “2-5G-3J”
 
 // Explanation: The string s has been split into three parts, each part has 2 characters except the first part, as it could be shorter as mentioned above
+
+function reformatString(S, K) {
+    if (!S || S.length === 0 || K <= 0) {
+        return "";
+    }
+
+    const groups = S.replace(/^=+|-+$/g, "").split("-");
+
+    let result = "";
+
+    for( let i=0;i< groups.length; i++) {
+        let group = groups[i].toUpperCase();
+
+        if (group.length>K) {
+            let chunks = [];
+
+            for(let j=0; j<group.length; j+=K) {
+                chunks.push(groups.substring(j,j+K));
+            }
+            group = chunks.join("-");
+        }
+
+        result += group;
+
+        if(i<groups.length-1) {
+            result += "-";
+        }
+    }
+
+    const firstGroupLength = result.indexOf("-");
+    const firstGroup = firstGroupLength === -1 ? result:result.substring(0,firstGroupLength);
+
+    if(firstGroupLength === -1 && result.length > K) {
+        let chunks = [];
+        for (let j=0;j<result.length;j+=K) {
+            chunks.push(result.substring(j,j+k))
+        }
+        result = chunks.join("-");
+    } else if(firstGroupLength !==-1 && firstGroup.length === 0) {
+        result = result.substring(result.indexOf("-")+1);
+    }
+
+    return result;
+}
+
+console.log(reformatString("5F3Z-2e-9-w"));
+console.log(reformatString("2-5g-3-J"));
